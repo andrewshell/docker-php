d1adabfd389a11f2a64c9cd7465c5c04d81e3a37
Andrew Shell <andrew@andrewshell.org>
2023-06-19 19:00:48 -0500

    Updating mysql and php, adding composer autoloading

--- index.php
+++ index.php
@@ -1,15 +1,15 @@
 <?php
+require __DIR__ . '/vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->safeLoad();
 
 try {
     echo 'Current PHP version: ' . phpversion();
     echo '<br />';
 
-    $host = 'db';
-    $dbname = 'database';
-    $user = 'user';
-    $pass = 'pass';
-    $dsn = "mysql:host=$host;dbname=$dbname;charset=utf8";
-    $conn = new PDO($dsn, $user, $pass);
+    $dsn = "mysql:host={$_ENV['DB_HOST']};port={$_ENV['DB_PORT']};dbname={$_ENV['DB_DATABASE']};charset=utf8";
+    $conn = new PDO($dsn, $_ENV['DB_USERNAME'], $_ENV['DB_PASSWORD']);
 
     echo 'Database connected successfully';
     echo '<br />';
